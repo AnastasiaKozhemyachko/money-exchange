6e1c65568d1a78d5198432ad17d3a03fac73515c
AnastasiaKozhemyachko <anastasiakozhemyachko@gmail.com>
2019-02-22 00:54:54 +0300

    Task done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,54 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+   var h = 0, q = 0, d = 0, n = 0, p = 0;
+    var obj = {
+      };
+      
+
+
+    // var str='{';
+if (currency>=10001) {
+        obj["error"] = "You are rich, my friend! We don\'t have so much coins for exchange";
+    }
+    else{
+        while (currency < 10001 && currency >= 50) {
+            h++;    
+            currency = currency-50;
+        }
+        if(h!=0){
+            obj["H"] = h;
+        }
+        while (currency < 50 && currency >= 25) {
+            q++;   
+            currency = currency-25; 
+        }
+        if(q!=0){
+            obj["Q"] = q;
+        }
+
+        while (currency < 25 && currency >= 10) {
+            d++;    
+            currency = currency-10;
+        }
+        if(d!=0){
+            obj["D"] = d;
+        }
+        
+        while (currency < 10 && currency >= 5) {
+            n++;    
+            currency = currency-5;
+        }    
+        if(n!=0){
+            obj["N"] = n;
+        }
+        while (currency < 5 && currency >= 1) {
+            p++;  
+            currency = currency-1;  
+        }
+        if(p!=0){
+            obj["P"] = p;
+        }
+    }
+    
+return obj;
 }
